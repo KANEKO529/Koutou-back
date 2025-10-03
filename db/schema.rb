b997222a147f5dd07d95710fb36df12470991f72
KANEKO529 <kanekohiromasa529@gmail.com>
2025-10-03 15:26:52 +0900

    fix: スキーマファイルからスキーマ定義をコメントアウト

--- db/schema.rb
+++ db/schema.rb
@@ -1,24 +1,12 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema[8.0].define(version: 2025_09_22_111205) do
-  create_schema "auth"
-  create_schema "extensions"
-  create_schema "graphql"
-  create_schema "graphql_public"
-  create_schema "pgbouncer"
-  create_schema "realtime"
-  create_schema "storage"
-  create_schema "vault"
+  # create_schema "auth"
+  # create_schema "extensions"
+  # create_schema "graphql"
+  # create_schema "graphql_public"
+  # create_schema "pgbouncer"
+  # create_schema "realtime"
+  # create_schema "storage"
+  # create_schema "vault"
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "extensions.pg_stat_statements"
