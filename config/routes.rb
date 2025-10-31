27852aa2eeb5129f751cfe9382be35a5a81237c7
KANEKO529 <kanekohiromasa529@gmail.com>
2025-10-31 18:30:41 +0900

    feat:toggle_publishの実装(routes.rb)

--- config/routes.rb
+++ config/routes.rb
@@ -16,16 +16,6 @@ Rails.application.routes.draw do
 
         # 管理者用API
       namespace :admin do
-        get "tags/index"
-        get "tags/show"
-        get "tags/create"
-        get "tags/update"
-        get "tags/destroy"
-        get "announcements/index"
-        get "announcements/show"
-        get "announcements/create"
-        get "announcements/update"
-        get "announcements/destroy"
         get 'rising-items', to: 'rising_items#index' # GET /api/v1/admin/rising-items
         get 'rising-items/:id', to: 'rising_items#show' # GET /api/v1/admin/rising-items/:id
         post 'rising-items', to: 'rising_items#create'# POST /api/v1/admin/rising-items/:id    
@@ -34,40 +24,26 @@ Rails.application.routes.draw do
         # キーワード検索
         # get 'rising-items?keyword=', to: 'rising_items#index' # GET /api/v1/admin/rising-items
 
-        resources :items, only: [:index, :show, :create, :update, :destroy]
         # resources :users, only: [:index, :update] # 優先度低git push -u origin feature/ブランチ名
 
         resources :announcements, only: [:index, :show, :create, :update, :destroy]
+        put 'announcements/:id/toggle-publish', to: 'announcements#toggle_publish'       # PUT /api/v1/risings/:id
+
         resources :tags, only: [:index, :show, :create, :update, :destroy]
+
       end
 
-      
       namespace :items do
-
         resources :items, only: [:index, :show, :create, :update, :destroy]
-
-        # get '', to: 'items#index'           # GET /api/v1/items
-        # get ':id', to: 'items#show'         # GET /api/v1/items/:id
-        # post '', to: 'items#create'         # POST /api/v1/items
-        # put ':id', to: 'items#update'       # PUT /api/v1/items/:id
-        # delete ':id', to: 'items#destroy'   # DELETE /api/v1/items/:id
       end
 
       namespace :risings do
 
         resources :risings, only: [:index, :show, :create, :update, :destroy]
-
-        # get '', to: 'risings#index'           # GET /api/v1/risings
-        # get ':id', to: 'risings#show'         # GET /api/v1/risings/:id
-        # post '', to: 'risings#create'         # POST /api/v1/risings
-        # put ':id', to: 'risings#update'       # PUT /api/v1/risings/:id
-        # delete ':id', to: 'risings#destroy'   # DELETE /api/v1/risings/:id
         put ':id/toggle-display', to: 'risings#toggle_display'       # PUT /api/v1/risings/:id
       end
 
       namespace :public do
-        # get "announcements/index"
-        # get "announcements/show"
         get 'rising-items', to: 'rising_items#index'  # GET /api/v1/public/risings-items
         get 'rising-items/:id', to: 'rising_items#show' # GET /api/v1/public/risings-items/:id
 
