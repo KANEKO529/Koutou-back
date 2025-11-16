c9726c7660127ecee4be6f7ac253fa70199efe74
KANEKO529 <kanekohiromasa529@gmail.com>
2025-11-16 19:44:17 +0900

    v1.11.0/feat モデルファイル作成

--- app/models/recommend_article.rb
+++ app/models/recommend_article.rb
@@ -1,2 +1,6 @@
 class RecommendArticle < ApplicationRecord
-end
+    has_many :featured_articles, dependent: :destroy
+    has_many :tags, through: :featured_articles
+  
+    validates :article_url, presence: true, uniqueness: true
+  end  
