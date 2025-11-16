1d7ce497221a33e0ca74b22fcc3ca95c61ea58fb
KANEKO529 <kanekohiromasa529@gmail.com>
2025-11-16 20:56:23 +0900

    v1.11.0/feat コントローラファイル実装

---
+++ app/controllers/api/v1/public/recommend_articles_controller.rb
@@ -0,0 +1,62 @@
+class Api::V1::Public::RecommendArticlesController < ApplicationController
+    def index
+        articles = RecommendArticle
+                    .includes(:featured_articles, :tags)
+                    .where(status: true)
+    
+        # section_name絞り込み
+        if params[:section_name].present?
+            articles = articles.joins(:featured_articles).where(featured_articles: { section_name: params[:section_name] }) 
+        end
+
+    
+        # tag_name絞り込み
+        if params[:tag_name].present?
+            articles = articles.joins(:tags).where(tags: { name: params[:tag_name] })
+        end
+    
+        # 並び順
+        articles = articles.left_joins(:featured_articles)
+                            .order('featured_articles.position ASC NULLS LAST, recommend_articles.created_at DESC')
+    
+        render json: {
+            status: "success",
+            data: articles.map { |a| serialize_article(a) }
+        }
+    end
+  
+    private
+  
+    def serialize_article(article)
+    
+        #ここで取得
+        meta = Rails.cache.fetch("meta:#{article.article_url}", expires_in: 12.hours) do
+            MetaFetcherService.fetch(article.article_url)
+        end
+    
+        {
+            article_id: article.id,
+            article_url: article.article_url,
+            title: meta[:title] || "タイトル情報なし",
+            description: meta[:description],
+            image_url: meta[:image_url],
+            tags: article.tags.pluck(:name),
+            author: {
+            id: article.created_by_author_id,
+            name: "管理チーム"
+            }
+        }
+    rescue => e
+      Rails.logger.error("[RecommendArticles] Meta fetch failed for #{article.article_url}: #{e.message}")
+      {
+        article_id: article.id,
+        article_url: article.article_url,
+        title: "取得エラー",
+        description: "この記事の情報を取得できませんでした",
+        image_url: nil,
+        tags: article.tags.pluck(:name),
+        author: { id: article.created_by_author_id, name: "管理チーム" }
+      }
+    end
+  end
+  
--- app/models/featured_article.rb
+++ app/models/featured_article.rb
@@ -2,8 +2,8 @@ class FeaturedArticle < ApplicationRecord
     belongs_to :recommend_article
     belongs_to :tag, optional: true
   
-    enum section_name: {
-      top_recommendation: 'top_recommendation'
+    enum :section_name, {
+        top_recommendation: 'top_recommendation'
     }
   
     validates :position, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
